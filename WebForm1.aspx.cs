f285cd144dc909552d1f64de1fad1327cd53e5d4
Tushar530 <tusharsoni95@gmail.com>
2018-10-05 14:12:29 +0530

    Add files via upload

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP_SEARCH_MASTERPAGE
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+        }
+
+        protected void btn_Search_Click(object sender, EventArgs e)
+        {
+            SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["cn1"].ConnectionString);
+
+
+            SqlDataReader dr;
+            SqlCommand cmd = new SqlCommand("[Tushar].[USP_ValidateEmail_Tushar_158230]", cn);
+
+
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+
+
+            cmd.Parameters.AddWithValue("@Email", txt_Email.Text);
+
+            cn.Open();
+
+            try
+
+            {
+
+                dr = cmd.ExecuteReader();
+               
+                    if (dr.HasRows)
+
+                    {
+
+
+                    dr.Read();
+                   
+
+                        string id = dr[3].ToString();
+
+                        Session["user"] = id;
+
+                        string Name = dr[0].ToString();
+
+                        Session["user1"] = Name;
+
+                        string Location = dr[1].ToString();
+
+                        Session["user2"] = Location;
+
+                        string Phone = dr[2].ToString();
+
+                        Session["user3"] = Phone;
+
+                        Response.Redirect("WebForm2.aspx");
+
+                    }
+
+
+
+                    else
+
+                        lbl_message.Text = "Invalid User";
+
+                }
+            
+
+            catch (Exception ex)
+
+            {
+
+                lbl_message.Text = ex.Message.ToString();
+
+            }
+
+            finally
+
+            {
+
+                cn.Close();
+
+            }
+
+        }
+    }
+}
---
+++ WebForm2.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP_SEARCH_MASTERPAGE
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+
+            {
+
+                lbl_Mess.Text = Session["user"].ToString();
+
+                txt_Email.Text = Session["user"].ToString();
+
+                txt_name.Text = Session["user1"].ToString();
+
+                txt_Location.Text = Session["user2"].ToString();
+
+                txt_phone.Text = Session["user3"].ToString();
+
+
+
+            }
+        }
+
+        protected void btn_update_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["cn1"].ConnectionString);
+
+            SqlCommand cmd = new SqlCommand("[Tushar].[USP_UpdateCustomer_TusharRAM]", cn);
+
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@Email", txt_Email.Text);
+
+            cmd.Parameters.AddWithValue("@CustomerName", txt_name.Text);
+
+            cmd.Parameters.AddWithValue("@Location", txt_Location.Text);
+
+            cmd.Parameters.AddWithValue("@PhoneNo", txt_phone.Text);
+
+
+
+
+
+
+
+            cn.Open();
+
+
+
+            cmd.ExecuteNonQuery();
+
+
+
+
+
+
+
+            try
+
+
+
+            {
+
+
+
+                lbl_Mess.Text = "Successfully Updated";
+
+
+
+            }
+
+
+
+            catch (Exception ex)
+
+
+
+            {
+
+
+
+                lbl_Mess.Text = ex.Message.ToString();
+
+
+
+            }
+
+
+
+
+
+
+
+            finally
+
+
+
+            {
+
+
+
+                cn.Close();
+
+
+
+            }
+        }
+    }
+}
